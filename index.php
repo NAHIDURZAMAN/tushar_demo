1d1a7b1d03af3d4d9b3c95d618d53800c916845e
Nahidur Zaman Tushar <127010775+NAHIDURZAMAN@users.noreply.github.com>
2025-05-27 16:45:28 +0600

    index php add

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>
+        <?php
+        echo "Welcome to my website";
+        ?>
+    </h1>
+</body>
+</html>
